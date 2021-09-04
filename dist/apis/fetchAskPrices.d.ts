6508e7fd087ec79e3d011d66ef7b475f2f92817b
Mario Kranjec <mariokranjec91@gmail.com>
2021-09-04 20:32:11 +0200

    fix: semantic-release/git

--- dist/apis/fetchAskPrices.d.ts
+++
@@ -1,3 +0,0 @@
-import { GetAskPriceResponse } from "../interfaces/Price";
-declare const _default: (product: string, currency?: string, limit?: number, page?: number) => Promise<GetAskPriceResponse>;
-export default _default;
--- dist/apis/fetchSales.d.ts
+++
@@ -1,3 +0,0 @@
-import { GetSalesResponse } from "../interfaces/Price";
-declare const _default: (product: string, currency?: string, limit?: number, page?: number) => Promise<GetSalesResponse>;
-export default _default;
